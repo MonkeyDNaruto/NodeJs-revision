991db5ec55311ae636be74367c9f014728524c72
MonkeyDNaruto <nischalshrestha324@gmail.com>
2022-12-30 16:33:07 +0545

    README.MD file added

--- app.js
+++ app.js
@@ -6,9 +6,9 @@ const app = express();
 // Setup Static And MiddleWare
 app.use(express.static("./02-express-tutorial/Public"))
 
-app.get('/', (req, res) => {
-    res.sendFile(path.resolve(__dirname, "./02-express-tutorial/navbar-app/index.html"))
-});
+// app.get('/', (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "./02-express-tutorial/navbar-app/index.html"))
+// });
 
 app.listen(3000, (req, res) => {
     console.log("server is listening on port: 3000");
